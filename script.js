3b5f5a47d1c3995316d59f44384d7af72734989c
Adler Koneski <adlerkoneski@hotmail.com>
2025-04-22 19:52:40 -0300

    atualizar total de gastos

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ const li = document.createElement('li');
 li.textContent = `${descricao}: R$ ${valor.toFixed(2)} (${categoria})`;
 const listaGastos = document.getElementById('lista-gastos');
 listaGastos.appendChild(li);
+
+const totalGastos = document.getElementById('total-gastos');
+const total = parseFloat(totalGastos.textContent) + valor;
+totalGastos.textContent = total.toFixed(2);
