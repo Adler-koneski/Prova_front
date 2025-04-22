938cb25f4f791ae9fdbbaae9b979afcce783bfef
Adler Koneski <adlerkoneski@hotmail.com>
2025-04-22 19:44:36 -0300

    pegar dados do formulario

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('gasto-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+const descricao = document.getElementById('descricao').value;
+const valor = parseFloat(document.getElementById('valor').value);
+const categoria = document.getElementById('categoria').value;
